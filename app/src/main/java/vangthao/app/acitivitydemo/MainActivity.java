050c86e68ad43cfa0002613c025f69f73771f0f7
thaoavang0042 <vang.thao@tda.company>
2020-12-28 17:28:50 +0700

    Create project

---
+++ app/src/main/java/vangthao/app/acitivitydemo/MainActivity.java
@@ -0,0 +1,14 @@
+package vangthao.app.acitivitydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
